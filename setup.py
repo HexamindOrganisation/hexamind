0838e0f5371290ab7909abba2adaf28c016038e1
Julien Fresnel <julien.fresnel@icloud.com>
2024-03-06 12:44:03 +0100

    restructuration and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hexs_rag',
+    version='0.1',
+    packages=find_packages(exclude=['test', 'data']),
+    #install_requires
+)
+    
