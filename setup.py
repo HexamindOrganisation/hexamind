fa667cbc85f236c015e956374c8105149f018084
Julien Fresnel <julien.fresnel@icloud.com>
2024-03-06 12:46:56 +0100

    package setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='hexs_rag',
     version='0.1',
-    packages=find_packages(exclude=['test', 'data']),
+    packages=find_packages(),
     #install_requires
 )
     
