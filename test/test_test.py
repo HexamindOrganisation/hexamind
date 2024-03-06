f8ba9ce7c042d4a54793ee523ca4248ff28ca6be
Max <Max.beales@gmail.com>
2024-03-06 12:33:25 +0100

    add docs + CI/CD yml

---
+++ test/test_test.py
@@ -0,0 +1,8 @@
+# delete this file later it's literally just to test the tests
+
+
+def test_always_passes():
+    assert True, "This test is designed to pass and verify CI/CD workflow setup."
+
+def test_never_passes():
+    assert False, "This test is designed to fail and verify CI/CD workflow setup."
