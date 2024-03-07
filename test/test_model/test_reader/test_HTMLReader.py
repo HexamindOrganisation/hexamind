82623796498c9f9da1e748038d1452e290ce392d
Max <Max.beales@gmail.com>
2024-03-07 17:33:20 +0100

    add error test

--- test/test_model/test_reader/test_HTMLReader.py
+++ test/test_model/test_reader/test_HTMLReader.py
@@ -3,32 +3,35 @@ from hexs_rag.model.readers.HTMLreader import Reader_HTML
 import pytest
 
 
-def test_html_reader():
-    """
-    Check if the attributes are being set or not
-    """
+# fixtures allow us to define data that we can reuse for each test
+# here we define an instance of the html reader
+@pytest.fixture 
+def reader_instance():
     # Test data file paths
     example_file_path = "../hexs_rag/data/test_data/HTML5 Test Page.html"
     # Create an instance of Reader_HTML
     reader = Reader_HTML(example_file_path)
+    return reader
+
+
+def test_html_reader(reader_instance):
+    """
+    Check if the attributes are being set or not
+    """
     # Assert that the reader instance has a 'paragraphs' attribute
-    assert hasattr(reader, 'paragraphs'), "Reader_HTML object does not have a 'paragraphs' attribute"
-    for paragraph in reader.paragraphs:
+    assert hasattr(reader_instance, 'paragraphs'), "Reader_HTML object does not have a 'paragraphs' attribute"
+    for paragraph in reader_instance.paragraphs:
         assert paragraph.id_ is not None
         assert paragraph.page_id is not None
         assert paragraph.text is not None
 
-def test_first_row():
+def test_first_row(reader_instance):
     """
     Check if the first row has the correct values
     """
-    # Test data file paths
-    example_file_path = "../hexs_rag/data/test_data/HTML5 Test Page.html"
-    # Create an instance of Reader_HTML
-    reader = Reader_HTML(example_file_path)
-    assert reader.paragraphs[0].text == "Heading 1"
-    assert reader.paragraphs[0].page_id == 1
-    assert reader.paragraphs[0].id_ == 210
+    assert reader_instance.paragraphs[0].text == "Heading 1"
+    assert reader_instance.paragraphs[0].page_id == 1
+    assert reader_instance.paragraphs[0].id_ == 210
 
 def test_table_format(): # TODO
     """
@@ -42,9 +45,12 @@ def test_list_format(): # TODO
     """
     pass
 
-def test_error_handling(): # TODO
+def test_incorrect_file_format(): 
     """
     Check to break code
     """
-    pass
+    reader = Reader_HTML('../hexs_rag/data/test_data/business-financial-data-december-2023-quarter-csv.csv')
+    # if an error occurs the reader simply sets the paragraphs attribute to an empty list
+    assert reader.paragraphs == []
+
     
