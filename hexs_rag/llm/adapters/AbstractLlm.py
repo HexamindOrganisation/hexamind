1169338ef8d8aa3f29f7fecfff343cfa0ecf75b3
Max <Max.beales@gmail.com>
2024-03-08 11:30:09 +0100

    fix llm imports

--- hexs_rag/llm/adapters/AbstractLlm.py
+++ hexs_rag/llm/adapters/AbstractLlm.py
@@ -10,7 +10,7 @@ class ILlmClient(ABC):
     @abstractmethod
     def chat(self, model, message, temperature=0):
         pass
-
+        
     @abstractmethod
     def create_chat_message(self, role :str, content:str):
         pass
