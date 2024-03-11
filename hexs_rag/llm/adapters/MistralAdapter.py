269760bac561614c34fe14ae67686ad2cf2dcf76
Julien Fresnel <julien.fresnel@icloud.com>
2024-03-11 16:50:48 +0100

    error handling for MistralAdapter

--- hexs_rag/llm/adapters/MistralAdapter.py
+++ hexs_rag/llm/adapters/MistralAdapter.py
@@ -27,18 +27,28 @@ class MistralClientAdapter(ILlmClient):
 
 
     def chat(self, messages, temperature=0):
-        chat_response = self.client.chat(
-            model=self.model,
-            messages=messages,
-            temperature=temperature
-        )
+        try: 
+            chat_response = self.client.chat(
+                model=self.model,
+                messages=messages,
+                temperature=temperature
+            )
+        except Exception as e:
+            raise ValueError(f"Could not chat with Mistral: {e}")
+        
         return chat_response.choices[0].message.content
     
     def create_chat_message(self, role, content):
-        return ChatMessage(role=role, content=content)
+        try:
+            return ChatMessage(role=role, content=content)
+        except Exception as e:
+            raise ValueError(f"Could not create chat message for Mistral: {e}")
 
     def embeddings(self, input):
-        return self.client.embeddings(
-            model=self.embed_model,
-            inputs=input
-       )
+        try: 
+            return self.client.embeddings(
+                model=self.embed_model,
+                inputs=input
+        )
+        except Exception as e:
+            raise ValueError(f"Could not get embeddings from Mistral: {e}")
