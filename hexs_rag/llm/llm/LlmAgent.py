645ac3330f540389349dd398461459522e2b4037
Julien Fresnel <julien.fresnel@icloud.com>
2024-03-13 10:54:52 +0100

    cleaning llmagent class

--- hexs_rag/llm/llm/LlmAgent.py
+++ hexs_rag/llm/llm/LlmAgent.py
@@ -33,23 +33,8 @@ class LlmAgent:
     
     def send_request_to_llm(self, messages):
         return self.client.chat(messages=messages)
-    
-    def generate_paragraph(self, query: str, context: dict, histo: list[(str, str)], language='fr') -> str:
-        template = (f"You are a conversation bot designed to answer to the query from users."
-                    f"Your answer is based on the context delimited by triple backticks :\n ``` {context} ```\n"
-                    f"You are consistent and avoid redundancies with the rest of the initial conversation delimited by triple backticks :\n ``` {histo} ```\n"
-                    f"Your response shall be in {language} and shall be concise."
-                    f"You shall only provide the answer, nothing else before and after."
-                    f"Here is the query you are given :\n"
-                    f"``` {query} ```")
-        messages = [self.client.create_chat_message("user", template)]
-        response = self.send_request_to_llm(messages)
-        print("****************")
-        print(response)
-        print("----")
-        return str(response)
 
-    def generate_paragraph_v2(self, query: str, context: dict, histo: list[(str, str)], language='fr') -> str:
+    def generate_paragraph(self, query: str, context: dict, histo: list[(str, str)], language='fr') -> str:
         """generates the  answer"""
         template = (f"You are a conversation bot designed to answer to the query from users."
                     f"Here is the query to answer : {query} in french"
@@ -64,18 +49,6 @@ class LlmAgent:
         return str(response)
 
     def translate(self, text: str) -> str:
-        """translates"""
-        template = (f"Your task consists in translating in English the following text delimited by triple backticks: ``` {text} ```\n"
-                    f"If the text is already in English, just return it !\n"
-                    f"Your must not provide an answer to the text, just translate it.\n")
-        messages = [self.client.create_chat_message("user", template)]
-        response = self.send_request_to_llm(messages)
-        print("****************")
-        print(response)
-        print("----")
-        return str(response)
-
-    def translate_v2(self, text: str) -> str:
         """translates"""
         template = "Translate in english the text. If it is already in english, just return the text."
         messages = [self.client.create_chat_message("user", template)]
@@ -104,24 +77,7 @@ class LlmAgent:
         print("----")
         return str(response)
 
-    def summarize_paragraph(self, prompt : str, title_doc : str = '',title_para : str = ''):
-        max_tokens = 700
-        """summarizes the paragraph"""
-        template = (f"Your task consists in summarizing the paragraph of the document untitled ```{title_doc}```."
-                    f"The paragraph title is ```{title_para}```."
-                    f"Your response shall be concise and shall respect the following format:"
-                    f"<summary>"
-                    f"If you see that the summary that you are creating will not respect ```{max_tokens}``` tokens, find a way to make it shorter."
-                    f"The paragraph you need to summarize is the following :"
-                    f"{prompt}")
-        messages = [self.client.create_chat_message("user", template)]
-        response = self.send_request_to_llm(messages)
-        print("****************")
-        print(response)
-        print("----")
-        return str(response)
-
-    def summarize_paragraph_v2(self, prompt : str, title_doc : str = '', title_para : str = ''):
+    def summarize_paragraph(self, prompt : str, title_doc : str = '', title_para : str = ''):
         max_tokens = 850
         location_of_the_paragraph = prompt.split(" :")[0]
         """summarizes the paragraph"""
@@ -138,30 +94,6 @@ class LlmAgent:
         return str(response)
 
     def detect_language(self, text: str) -> str:
-        """detects the language"""
-        template = (f"Your task consists in detecting the language of the last question or sentence of the text."
-                    f"You should only give the two letters code of the language detected, nothing else."
-                    f"Here is the text you are given delimited by triple backticks : ```{text}```")
-        messages = [self.client.create_chat_message("user", template)]
-        response = self.send_request_to_llm(messages)
-        print("****************")
-        print(response)
-        print("----")
-        return str(response)
-    
-    def detect_language_v2(self, text: str) -> str:
-        """detects the language"""
-        template = (f"Your task consists in detecting the language of the last question or sentence of the text."
-                f"You should only give the two letters code of the language detected, nothing else.")
-        messages = [self.client.create_chat_message("user", template)]
-        response = self.send_request_to_llm(messages)
-        print("****************")
-        print(response)
-        print("----")
-        return str(response)
-
-
-    def detect_language_v2(self, text: str) -> str:
         """detects the language"""
         template = (f"Your task consists in detecting the language of the last question or sentence of the text."
                 f"You should only give the two letters code of the language detected, nothing else.")
