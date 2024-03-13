e164c624ad67e5a51c4d2bbe39ff922f9bd5afc7
Julien Fresnel <julien.fresnel@icloud.com>
2024-03-13 11:12:06 +0100

    reorganization and cleaning

--- hexs_rag/llm/llm/LlmAgent.py
+++ hexs_rag/llm/llm/LlmAgent.py
@@ -1,5 +1,6 @@
 import os
 from hexs_rag.llm.adapters.AbstractLlm import ILlmClient
+from hexs_rag.utils.llm.template import Template
 
 class LlmAgent:
     def __init__(self, client : ILlmClient):
@@ -36,70 +37,49 @@ class LlmAgent:
 
     def generate_paragraph(self, query: str, context: dict, histo: list[(str, str)], language='fr') -> str:
         """generates the  answer"""
-        template = (f"You are a conversation bot designed to answer to the query from users."
-                    f"Here is the query to answer : {query} in french"
-                    f"Your answer is based on the context delimited by triple backticks :\n ``` {context} ```\n and your personal knowledge"
-                    f"You are consistent and avoid redundancies with the rest of the initial conversation delimited by triple backticks :\n ``` {histo} ```\n"
-                    f"Your response shall be in french and shall be concise.")
+        template = Template.generate_paragraph(query, context, histo)
         messages = [self.client.create_chat_message("user", template)]
         response = self.send_request_to_llm(messages)
-        print("****************")
-        print(response)
-        print("----")
         return str(response)
 
     def translate(self, text: str) -> str:
         """translates"""
-        template = "Translate in english the text. If it is already in english, just return the text."
+        template = Template.translate(text)
         messages = [self.client.create_chat_message("user", template)]
         response = self.send_request_to_llm(messages)
-        print("****************")
-        print(response)
-        print("----")
         return str(response)
 
     def generate_answer(self, query: str, answer: str, histo: str, context: str,language : str) -> str:
         """provides the final answer in {language} based on the initial query and the answer in english"""
-        template = (f"Your task consists in translating the answer in {language}, if its not already the case, to the query "
-                    f"delimited by triple backticks: ```{query}``` \n"
-                    f"You don't add new content to the answer but: "
-                    f"1 You can use some vocabulary from the context delimited by triple backticks:\n"
-                    f"```{context}```\n"
-                    f"2 You are consistent and avoid redundancies with the rest of the initial"
-                    f"conversation delimited by triple backticks: ```{histo}```\n"
-                    f"Your response shall respect the following format:<response>\n"
-                    f"Here is the answer you are given in {language}:"
-                    f"{answer}")
+        template = Template.generate_answer(query, answer, histo, context, language)
         messages = [self.client.create_chat_message("user", template)]
         response = self.send_request_to_llm(messages)
-        print("****************")
-        print(response)
-        print("----")
         return str(response)
 
     def summarize_paragraph(self, prompt : str, title_doc : str = '', title_para : str = ''):
-        max_tokens = 850
         location_of_the_paragraph = prompt.split(" :")[0]
         """summarizes the paragraph"""
-        template = (f"Your task consists in summarizing in English the paragraph of the document untitled ```{title_doc}``` located in the ```{location_of_the_paragraph}``` section of the document."
-                    f"The paragraph title is ```{title_para}```."
-                    f"Your response shall be concise and shall respect the following format:"
-                    f"<summary>"
-                    f"If you see that the summary that you are creating will not respect ```{max_tokens}``` tokens, find a way to make it shorter.")
+        template = Template.summarize_paragraph(location_of_the_paragraph, title_doc, title_para)
         messages = [self.client.create_chat_message("user", template)]
         response = self.send_request_to_llm(messages)
-        print("****************")
-        print(response)
-        print("----")
         return str(response)
 
     def detect_language(self, text: str) -> str:
         """detects the language"""
-        template = (f"Your task consists in detecting the language of the last question or sentence of the text."
-                f"You should only give the two letters code of the language detected, nothing else.")
+        template = Template.detect_language(text)
         messages = [self.client.create_chat_message("user", template)]
         response = self.send_request_to_llm(messages)
+        return str(response)
+    
+    def get_embedding(self, text):
+        """
+        Returns text sembeddings 
+        """
+        embeddings_batch_response = self.llmagent.client.embeddings(input=[text])
+        return embeddings_batch_response.data[0].embedding
+
+    @staticmethod
+    def print_response(self, response):
         print("****************")
         print(response)
         print("----")
-        return str(response)
