910dff4e8205210e88878135d346801080cab9ac
Julien Fresnel <julien.fresnel@hexamind.ai>
2024-03-21 10:25:53 +0100

    adding some utils

---
+++ test.py
@@ -0,0 +1,7 @@
+from hxm_rag.model.modelv2.document import Document
+from hxm_rag.model.modelv2.container import Container
+from hxm_rag.model.modelv2.block import Block
+from hxm_rag.model.readersv2.PdfReader import PdfReader
+
+if __name__ == "__main__":
+    pdf = PdfReader('data/SGFGAS/Epargne-logement phase épargne.pdf')
