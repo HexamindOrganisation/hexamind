2d6f786b1ecebc1920be1c99dd4d18eeb06bd506
Julien Fresnel <julien.fresnel@hexamind.ai>
2024-06-05 14:58:49 +0200

    Refactor hxm_rag to hexamind library

--- test.py
+++
@@ -1,7 +0,0 @@
-from hexamind.model.model.document import Document
-from hexamind.model.model.container import Container
-from hexamind.model.model.block import Block
-from hexamind.model.readers.WordReader import WordReader
-
-if __name__ == "__main__":
-    doc = WordReader('data/SGFGAS/Epargne-logement phase épargne.docx')
