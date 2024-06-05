6fc15cc171a880a2814f7a04b496043c54a219b9
Julien Fresnel <julien.fresnel@hexamind.ai>
2024-06-05 16:05:24 +0200

    Refactor file paths in _env.py and pyproject.toml

--- hexamind/utils/utils/_env/_env.py
+++ hexamind/utils/utils/_env/_env.py
@@ -13,7 +13,7 @@ def setup_env_file(destination_directory=None):
         destination_directory.mkdir(parents=True, exist_ok=True)
 
     template_file = pkg_resources.resource_filename(
-        __name__, "hexamind/utils/utils/_env/.env.template"
+        __name__, ".env.template"
     )
     env_file = destination_directory / ".env"
 
