5e0c6c3137ecbdd3bdb6653de4d2cfe60c95420b
Julien Fresnel <julien.fresnel@hexamind.ai>
2024-06-06 14:51:59 +0200

    feat: Refactor HtmlReader and MkTransformer classes
    The commit refactors the HtmlReader and MkTransformer classes. It removes the unused `table_to_markdown` method from the HtmlReader class and renames it to `_table_to_markdown`. It also adds the MkTransformer class, which includes methods for converting markdown content to nested dict strcuture. The commit improves the code organization and readability.

--- hexamind/model/readers/HtmlReader.py
+++ hexamind/model/readers/HtmlReader.py
@@ -3,33 +3,11 @@ import requests
 from hexamind.model.readers.IReader import IReader
 
 class HtmlReader(IReader):
-    """
-    This class reads a HTLM document and extracts its structure into a nested dictionary.
 
-    The structure of the dictionary is as follows:
-    {
-        'type' : 'container' or 'block',
-        'children' : [list of nested dictionaries],
-        'content' : None or string,
-        'level' : int,
-        'parent' : parent dictionary
-    }
-
-    The 'type' key indicates if the dictionary represents a container or a block.
-
-    The 'children' key contains a list of nested dictionaries that represent the children of the current dictionary.
-
-    The 'content' key contains the text content of the block or container. If the dictionary represents a container, the content is None.
-
-    The 'level' key contains the level of the block or container in the hierarchy.
-
-    The 'parent' key contains a reference to the parent dictionary in the hierarchy.
-
-    """
     def __init__(self, html_content):
         self.soup = BeautifulSoup(html_content, 'html.parser')
     
-    def table_to_markdown(self, table):
+    def _table_to_markdown(self, table):
         table_content = ''
         for row in table.find_all('tr'):
             row_text = ' | '.join(cell.get_text(strip=True) for cell in row.find_all(['td', 'th']))
@@ -47,7 +25,7 @@ class HtmlReader(IReader):
                 if text:
                     markdown_content += f"{text}\n\n"
             elif element.name == 'table':
-                table_md = self.table_to_markdown(element)
+                table_md = self._table_to_markdown(element)
                 markdown_content += f"{table_md}\n\n"
 
         return markdown_content
