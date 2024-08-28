90f3eed46bef49762fb0290611e2026bfbe67f48
AlexandreFleutelot <fleutelot.alexandre@gmail.com>
2024-08-28 11:24:23 +0200

    Pending Publisher

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='hexamind',
     version='0.1',
-    author='Julien Fresnel, Max Beales',
-    author_email='julien.fresnel@hexamind.ai, max.beales@hexamind.ai',
-    description='Hexamind library to implement RAG solutions',
+    author='Julien Fresnel, Max Beales, Alexandre Fleutelot',
+    author_email='julien.fresnel@hexamind.ai, max.beales@hexamind.ai, alexandre.fleutelot@hexamind.ai',
+    description='Hexamind library',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
+    classifiers=[
+           "Programming Language :: Python :: 3",
+           "License :: OSI Approved :: MIT License",
+           "Operating System :: OS Independent",
+       ],
     entry_points={
         'console_scripts': [
             'hexamind-env=hexamind.cli:main'
